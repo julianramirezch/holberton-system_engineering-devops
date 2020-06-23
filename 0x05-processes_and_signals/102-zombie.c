c4710fd86752c221e61deac05eae158f1c552476
julianramirezch <julianramirezch1@gmail.com>
2020-06-22 21:06:39 -0500

    [add]:  C program that creates 5 zombie processes.

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - Sylvain Function
+ * Return: 0 on success
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Function create 5 zombie process
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	pid_t pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			printf("Zombie process created, PID: %i\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
