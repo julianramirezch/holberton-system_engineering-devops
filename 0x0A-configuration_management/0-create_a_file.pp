57e13c0ee1293fb810b2f6aad7b366f9563af6ac
julianramirezch <julianramirezch1@gmail.com>
2020-08-03 21:14:04 +0000

    [add]: Create file holberton with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file with requeriments
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
