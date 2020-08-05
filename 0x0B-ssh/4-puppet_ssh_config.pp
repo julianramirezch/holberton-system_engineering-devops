41b45b5cbf4431c5910f202094d0e45046e7e16b
julianramirezch <julianramirezch1@gmail.com>
2020-08-05 15:09:40 +0000

    [add]: set up your client SSH configuration file

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#Configuration File
+
+file { 'holberton_server:
+    ensur => file,
+    content => 'Host holberton_server
+                    HostName 34.73.118.163
+                    User ubuntu
+                    IdentityFile ~/.ssh/holberton
+                    PasswordAuthentication no',
+}
+
