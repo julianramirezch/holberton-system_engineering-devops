19477c7694e77ab769793f69b92ea45b6f12e672
julianramirezch <julianramirezch1@gmail.com>
2020-08-05 20:50:23 -0500

    [install]: package nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,9 @@
 # Puppet configuration
 
-exec {'/usr/bin/env apt-get -y update': }
-exec {'/usr/bin/env apt-get -y install nginx': }
+package { 'nginx':
+    ensure => installed,
+}
+
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.millonarios.com.co permanent;" /etc/nginx/sites-available/default':}
 exec {'/usr/bin/env service nginx start':}
