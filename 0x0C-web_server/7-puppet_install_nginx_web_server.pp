a3783a0060fd7e9d32ceee842de6af5cbaee536e
julianramirezch <julianramirezch1@gmail.com>
2020-08-05 20:45:55 -0500

    [change]: change commands apt

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,7 @@
 # Puppet configuration
 
-exec { 'update':
-    command => '/usr/bin/env apt-get -y update',
-}
-
-exec { 'install':
-    command => '/usr/bin/env apt-get -y install nginx',
-}
-
+exec { '/usr/bin/env apt-get -y update': }
+exec { '/usr/bin/env apt-get -y install nginx':}
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.millonarios.com.co permanent;" /etc/nginx/sites-available/default':}
 exec {'/usr/bin/env service nginx start':}
