bcacba4c86d9111401d7afea2c438c5270cd22bb
julianramirezch <julianramirezch1@gmail.com>
2020-08-05 20:11:12 -0500

    [fix]:quotes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,5 +9,5 @@ package { 'nginx':
 }
 
 exec { 'configuration':
-    command => '/usr/bin/echo "Holberton School" | sudo tee /var/www/html/index.html'; '/usr/bin/sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.millonarios.com.co permanent;" /etc/nginx/sites-available/default'; service nginx start,
+    command => '/usr/bin/echo "Holberton School" | sudo tee /var/www/html/index.html; sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.millonarios.com.co permanent;" /etc/nginx/sites-available/default; service nginx start',
 }
