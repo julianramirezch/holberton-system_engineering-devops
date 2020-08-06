369a95b4c71be5005f4ade10eea88d18ba1b8633
julianramirezch <julianramirezch1@gmail.com>
2020-08-05 20:33:32 -0500

    [change]: path /usr/bin/env

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,6 +8,6 @@ package { 'nginx':
     ensure => installed,
 }
 
-exec { '/usr/bin/echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
-exec {'/usr/bin/sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.millonarios.com.co permanent;" /etc/nginx/sites-available/default':}
-exec {'/usr/sbin/service nginx restart':}
+exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.millonarios.com.co permanent;" /etc/nginx/sites-available/default':}
+exec {'/usr/bin/env service nginx restart':}
