f9e3983b4a29452dc4a483aa789c9c307b2cf1aa
julianramirezch <julianramirezch1@gmail.com>
2020-09-14 16:54:15 -0500

    [refactor]

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
 ''' Python script that, using a REST API, for a given employee ID, returns
-information about his/her todo list progress '''
+information about his/her TODO list progress '''
 import requests
-import sys
+from sys import argv
 
 
-def make_request(todo, empl, user_id):
+def make_request(todo, empl):
     ''' Make request to url '''
     titles = []
-    completed = 0
-    tasks = 0
     response_todo = requests.get(todo)
     if response_todo.status_code == 200:
+        completed = 0
         for task, i in enumerate(response_todo.json()):
-            if i.get('userId') == user_id:
-                tasks += 1
-                if i.get('completed') is True:
-                    completed += 1
-                    titles.append(i.get('title'))
+            if i.get('completed') is True:
+                completed += 1
+                titles.append(i.get('title'))
 
     response_empl = requests.get(empl)
     if response_empl.status_code == 200:
         name = response_empl.json().get('name')
 
     print('Employee {} is done with tasks({}/{}):'.format(name, completed,
-          tasks))
+          task + 1))
     [print('\t {}'.format(title)) for title in titles]
 
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_id = argv[1]
+    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos/'.format(
+        user_id)
     empl_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    make_request(todo_url, empl_url, int(user_id))
+    make_request(todo_url, empl_url)
