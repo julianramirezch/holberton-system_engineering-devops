9b33273ec15a386029afdd6dbabd82817c55eb3f
julianramirezch <julianramirezch1@gmail.com>
2020-09-17 15:58:09 -0500

    [add]: function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Python script to extract top ten titles from subreddit '''
+import requests
+
+
+def top_ten(subreddit):
+    ''' prints the titles of the first 10 hot posts listed for a given
+    subreddit '''
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101\
+    Firefox/80.0'
+    headers = {
+        'User-Agent': ua,
+    }
+    url = 'https://www.reddit.com/r/{}/top/.json?=10'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        children = response.json().get('data').get('children')
+        for idx, i in enumerate(children):
+            if idx < 10:
+                print(i.get('data').get('title'))
